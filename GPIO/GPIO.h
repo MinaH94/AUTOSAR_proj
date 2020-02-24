0963e59f2f4901701c684103f810b71e76e34a46
Mina Helmi <mina.helmi.94@gmail.com>
2020-02-24 02:09:14 +0200

    GPIO: added APIs prototypes

--- GPIO/GPIO.h
+++ GPIO/GPIO.h
@@ -11,4 +11,8 @@
 #define GPIO_PORT_E 0x4005C000
 #define GPIO_PORT_F 0x4005D000
 
+Error_S GPIO_Init(u32 port, u8 pin, u8 direction);
+Error_S GPIO_ReadPin(u32 port, u8 pin, u8* out);
+Error_S GPIO_WritePin(u32 port, u8 pin, u8 value);
+
 #endif /* GPIO_H_ */
