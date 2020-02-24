128bf6e082ab364b3916faaa26c0fdb264aa26e8
Mahmoud Gamal <mahmouddgamall93@gmail.com>
2020-02-24 13:55:25 +0200

    Updating GPIO to read and write

--- GPIO/GPIO.c
+++ GPIO/GPIO.c
@@ -1,21 +1,25 @@
 /*
-GPIO Port A (AHB): 0x40058000
-GPIO Port B (AHB): 0x40059000
-GPIO Port C (AHB): 0x4005A000
-GPIO Port D (AHB): 0x4005B000
-GPIO Port E (AHB): 0x4005C000
-GPIO Port F (AHB): 0x4005D000
+GPIO In_u32Port A (AHB): 0x40058000
+GPIO In_u32Port B (AHB): 0x40059000
+GPIO In_u32Port C (AHB): 0x4005A000
+GPIO In_u32Port D (AHB): 0x4005B000
+GPIO In_u32Port E (AHB): 0x4005C000
+GPIO In_u32Port F (AHB): 0x4005D000
 */
 
 #include "STD_TYPES.h"
 #include "GPIO.h"
+#include "GPIO_Config.h"
 
 /* RCC module base */
 #define SYSTEM_CTRL_BASE (0x400FE000)
-/* Port clock control register */
+/* In_u32Port clock control register */
 #define RCGCGPIO         (*((volatile u32*)(SYSTEM_CTRL_BASE + 0x608)))
 
-/* Clock enable values */
+#define	SYSCTRL_GPIOHBCTL				(*(u32*)(SYSTEM_CTRL_BASE+0x06C))
+
+
+/* Clock enable In_u8Values */
 #define PORTA_CLOCK_ENABLE 0x00000001
 #define PORTB_CLOCK_ENABLE 0x00000002
 #define PORTC_CLOCK_ENABLE 0x00000004
@@ -23,14 +27,12 @@ GPIO Port F (AHB): 0x4005D000
 #define PORTE_CLOCK_ENABLE 0x00000010
 #define PORTF_CLOCK_ENABLE 0x00000020
 
-#define PIN_COUNT 8
+#define In_u8Pin_COUNT 8
 
 typedef struct
 {
-    u32 GPIODATA;
-
-    const u32 _padding1[255]; /* reserved area */
-
+		const u32 pad=0;
+    u32 GPIODATA[256];
     u32 GPIODIR;
     u32 GPIODATA;
     u32 GPIODIR;
@@ -77,33 +79,33 @@ typedef struct
 } GPIO_t;
 
 
-Error_S GPIO_Init(u32 port, u8 pin, u8 direction)
+Error_S GPIO_Init(u32 In_u32Port, u8 In_u8Pin, u8 direction)
 {
-    Error_S err;
+    u8 err;
     u32 dir;
     u32 pinMask;
 
-    /* if port value is incorrect */
-    if ((GPIO_PORT_A != port) &&
-        (GPIO_PORT_B != port) &&
-        (GPIO_PORT_C != port) &&
-        (GPIO_PORT_D != port) &&
-        (GPIO_PORT_E != port) &&
-        (GPIO_PORT_F != port))
+    /* if In_u32Port In_u8Value is incorrect */
+    if ((GPIO_PORT_A != In_u32Port) &&
+        (GPIO_PORT_B != In_u32Port) &&
+        (GPIO_PORT_C != In_u32Port) &&
+        (GPIO_PORT_D != In_u32Port) &&
+        (GPIO_PORT_E != In_u32Port) &&
+        (GPIO_PORT_F != In_u32Port))
     {
         err = ERROR_NOK;
     }
-    else if (PIN_COUNT <= pin) /* if pin value is outside range */
+    else if (In_u8Pin_COUNT <= In_u8Pin) /* if In_u8Pin In_u8Value is Out_u8Outside range */
     {
         err = ERROR_NOK;
     }
     else
     {
-        volatile GPIO_t* baseAddr = (volatile GPIO_t*)port;
-        pinMask = (u32)1 << pin;
+        volatile GPIO_t* baseAddr = (volatile GPIO_t*)In_u32Port;
+        PinMask = (u32)1 << In_u8Pin;
 
-        /* 1- enable port clock */
-        switch (port)
+        /* 1- enable In_u32Port clock */
+        switch (In_u32Port)
         {
         case GPIO_PORT_A:
             RCGCGPIO |= PORTA_CLOCK_ENABLE;
@@ -131,23 +133,27 @@ Error_S GPIO_Init(u32 port, u8 pin, u8 direction)
         
         }
 
-        /* 2- set direction (input or output) */
+        /* 2- set direction (input or Out_u8Output) */
         dir = baseAddr->GPIODIR;
-        dir &= ~pinMask;
-        dir |= direction << pin;
+        dir &= ~PinMask;
+        dir |= direction << In_u8Pin;
         baseAddr->GPIODIR = dir;
 
         /* 3- set mode to GPIO (not Alternate Function) */
-        baseAddr->GPIOAFSEL &= ~pinMask;
+        baseAddr->GPIOAFSEL &= ~pinmask;
 
         /* 4- turn pull up/down if input mode */
-        if (GPIO_PIN_DIR_INPUT == direction)
+        if (GPIO_In_u8Pin_DIR_INPUT == direction)
         {
-            baseAddr->GPIOPUR |= pinMask;
+#if GPIO_PULL_UP_DOWN == GPIO_PULL_UP
+            baseAddr->GPIOPUR |= pinmask;
+#elif GPIO_PULL_UP_DOWN == GPIO_PULL_DOWN
+            baseAddr->GPIOPDR |= pinmask;
+#endif
         }
 
         /* 5- enable digital I/O mode (not Analog mode) */
-        baseAddr->GPIODEN |= pinMask;
+        baseAddr->GPIODEN |= pinmask;
 
         err = ERROR_OK;
     }
@@ -157,5 +163,99 @@ Error_S GPIO_Init(u32 port, u8 pin, u8 direction)
 
 
 
+Error_S SYSCTRL_GPIOBusControl(u8 In_u8In_u32Port, u8 In_u8Bus)
+{
+	u8 Local_u8Status= ERROR_NOK;
+	if(SYSCTRL_APB == In_u8Bus)
+	{
+		Local_u8Status = ERROR_OK;
+		SYSCTRL_GPIOHBCTL &=~ In_u8In_u32Port
+	}
+	else if(SYSCTRL_AHB == In_u8Bus)
+	{
+		Local_u8Status = ERROR_OK;
+		SYSCTRL_GPIOHBCTL |= In_u8In_u32Port
+	}
+	return Local_u8Status;
+}
+
+Error_S GPIO_ReadIn_u8Pin(u32 In_u32Port, u8 In_u8Pin, u8* Out_u8Out)
+{
+	u8 err = ERROR_OK;
+	    if ((GPIO_PORT_A != In_u32Port) &&
+        (GPIO_PORT_B != In_u32Port) &&
+        (GPIO_PORT_C != In_u32Port) &&
+        (GPIO_PORT_D != In_u32Port) &&
+        (GPIO_PORT_E != In_u32Port) &&
+        (GPIO_PORT_F != In_u32Port))			
+    {
+        err = ERROR_NOK;
+    }
+    else if (
+							GPIO_PIN_0!= In_u8Pin) &&
+							GPIO_PIN_1!= In_u8Pin) &&
+							GPIO_PIN_2!= In_u8Pin) &&
+							GPIO_PIN_3!= In_u8Pin) &&
+							GPIO_PIN_4!= In_u8Pin) &&
+							GPIO_PIN_5!= In_u8Pin) &&		
+							GPIO_PIN_6!= In_u8Pin) &&
+							GPIO_PIN_7!= In_u8Pin) &&
+							GPIO_PIN_8!= In_u8Pin) &&
+							GPIO_PIN_9!= In_u8Pin) 					) /* if In_u8Pin In_u8Value is Out_u8Outside range */
+    {                  
+        err = ERROR_NOK;
+    }
+    else
+    {
+        volatile GPIO_t* baseAddr = (volatile GPIO_t*)In_u32Port;
+				/*	GPIODATA is mapped to 256 register that read and write operations are mapped to,
+						to understand more please go to page 654 in the manual */
+			if( 0 < baseAddr->GPIODATA[In_u8Pin] )
+			{
+				*Out_u8Out = 1;
+			}
+			else
+			{
+				*Out_u8Out = 0;
+			}
+		}
+	return err;
+}
+Error_S GPIO_WriteIn_u8Pin(u32 In_u32Port, u8 In_u8Pin, u8 In_u8Value)
+{
+	
+	u8 err = ERROR_OK;
+	    if ((GPIO_PORT_A != In_u32Port) &&
+        (GPIO_PORT_B != In_u32Port) &&
+        (GPIO_PORT_C != In_u32Port) &&
+        (GPIO_PORT_D != In_u32Port) &&
+        (GPIO_PORT_E != In_u32Port) &&
+        (GPIO_PORT_F != In_u32Port))			
+    {
+        err = ERROR_NOK;
+    }
+    else if (
+							GPIO_PIN_0!= In_u8Pin) &&
+							GPIO_PIN_1!= In_u8Pin) &&
+							GPIO_PIN_2!= In_u8Pin) &&
+							GPIO_PIN_3!= In_u8Pin) &&
+							GPIO_PIN_4!= In_u8Pin) &&
+							GPIO_PIN_5!= In_u8Pin) &&		
+							GPIO_PIN_6!= In_u8Pin) &&
+							GPIO_PIN_7!= In_u8Pin) &&
+							GPIO_PIN_8!= In_u8Pin) &&
+							GPIO_PIN_9!= In_u8Pin) 					) /* if In_u8Pin In_u8Value is Out_u8Outside range */
+    {                  
+        err = ERROR_NOK;
+    }
+    else
+    {
+				/*	GPIODATA is mapped to 256 register that read and write operations are mapped to,
+						to understand more please go to page 654 in the manual */
+      volatile GPIO_t* baseAddr = (volatile GPIO_t*)In_u32Port;
+			baseAddr->GPIODATA[In_u8Pin] = In_u8Value;
+		}
+	return err;
+}
 
 
--- GPIO/GPIO.h
+++ GPIO/GPIO.h
@@ -1,38 +1,53 @@
 #ifndef GPIO_H_
 #define GPIO_H_
 
-/* Pin direction: input or output */
 #define GPIO_PIN_DIR_OUTPUT 1
 #define GPIO_PIN_DIR_INPUT  0
-/* ****************************** */
 
-/* Port select: [A - F] */
 #define GPIO_PORT_A 0x40058000
 #define GPIO_PORT_B 0x40059000
 #define GPIO_PORT_C 0x4005A000
 #define GPIO_PORT_D 0x4005B000
 #define GPIO_PORT_E 0x4005C000
 #define GPIO_PORT_F 0x4005D000
-/* ****************************** */
 
-/* Pin number: [0 - 7] */
-#define GPIO_PIN_0 0
-#define GPIO_PIN_1 1
-#define GPIO_PIN_2 2
-#define GPIO_PIN_3 3
-#define GPIO_PIN_4 4
-#define GPIO_PIN_5 5
-#define GPIO_PIN_6 6
-#define GPIO_PIN_7 7
-/* ****************************** */
+#define	GPIO_PIN_0	0x1
+#define	GPIO_PIN_1	0x2
+#define	GPIO_PIN_2	0x4
+#define	GPIO_PIN_3	0x8
+#define	GPIO_PIN_4	0x10
+#define	GPIO_PIN_5	0x20
+#define	GPIO_PIN_6	0x40
+#define	GPIO_PIN_7	0x80
+#define	GPIO_PIN_8	0x100
+#define	GPIO_PIN_9	0x200
 
-/* Pin value: HIGH or LOW */
-#define GPIO_PIN_VALUE_HIGH 1
-#define GPIO_PIN_VALUE_LOW  0
-/* ****************************** */
+#define SYSCTRL_APB							0
+#define SYSCTRL_AHB							1
 
-Error_S GPIO_Init(u32 port, u8 pin, u8 direction);
-Error_S GPIO_ReadPin(u32 port, u8 pin, u8* out);
-Error_S GPIO_WritePin(u32 port, u8 pin, u8 value);
+
+#define GPIO_SET		0xFFFF
+
+#define GPIO_RESET		0x0
+
+Error_S GPIO_Init(u32 In_u32Port, u8 In_u8Pin, u8 In_u8Direction);
+/*
+SYSCTRL_APB
+SYSCTRL_AHB
+
+
+
+
+
+
+
+
+*/
+
+Error_S SYSCTRL_GPIOBusControl(u8 In_u8In_u32Port, u8 In_u8Bus);
+
+Error_S GPIO_ReadPin(u32 In_u32port, u8 In_u8pin, u8* Out_u8Out);
+
+Error_S GPIO_WritePin(u32 In_u32Port, u8 In_u8Pin, u8 In_u8Value);
 
 #endif /* GPIO_H_ */
